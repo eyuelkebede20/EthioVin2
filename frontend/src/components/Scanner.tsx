31e23dfb5b08c26ed31ea23250ce06a74b6985a7
fluffyweird <eyuduc@gmail.com>
2026-05-03 22:14:19 +0300

    fixed scanner.tsx by adding a form

--- frontend/src/components/Scanner.tsx
+++ frontend/src/components/Scanner.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { vinService, type ScanResponse } from "../api/vinService";
+
+import { scanVin, type ScanResponse } from "../api/vinService";
 
 const RegionToggle = ({ region, onRegionChange }: { region: string; onRegionChange: (r: string) => void }) => {
   return (
