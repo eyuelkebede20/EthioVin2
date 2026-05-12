514ca785089731e063ac6d3521ce3364805c1b60
fluffyweird <eyuduc@gmail.com>
2026-05-12 16:30:20 +0300

    typos

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "./schema";
+import * as schema from "./schema.ts";
 
 const connectionString = process.env.DATABASE_URL as string;
 
